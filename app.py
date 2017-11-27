930abab9c2e8591ad0a332f965775075c9f54bb6
Tushar Mittal <chiragmittal.mittal@gmail.com>
2017-11-28 00:08:11 +0530

    added all the files

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO,emit
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'donottellanyoneaboutthis'
+
+#socketio wrapper for the app
+socketio = SocketIO(app)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+#event for broadcasting message to everyone...
+@socketio.on('my event')
+def handel_message(json):
+    print(json)
+    socketio.emit('my response',json)
+
+if __name__ == '__main__':
+    socketio.run(app, debug = True)
